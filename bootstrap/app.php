2b9e7bc65ad3ff93f2e4f7b0c4c95a9d6d184095
luisdelgadoc10 <luisdsa.07@gmail.com>
2025-10-15 12:33:15 -0500

    cambio 4 en cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // ⚡ Configurar Sanctum para API
         $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
