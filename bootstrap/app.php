982bc77512fdae0f380c0eaa19a0e4077b2fe1e7
luisdelgadoc10 <luisdsa.07@gmail.com>
2026-01-06 15:05:19 -0500

    correccion 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->trustProxies(at: '*');
         // ⚡ Configurar Sanctum para API
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
