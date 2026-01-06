b4b16a70c715eaed12d9d2c8890425c2f3d57505
luisdelgadoc10 <luisdsa.07@gmail.com>
2026-01-06 10:39:32 -0500

    final

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:8001','https://tesis-frontend-seven.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:8001','https://tesis-frontend-seven.vercel.app','https://smartrisk-psi.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
